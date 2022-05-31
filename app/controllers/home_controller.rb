0f6133b0e3c9bbd39a38d2dc81eb843a262edcc1
bube-dev <nwajiuba02@gmail.com>
2022-05-31 16:45:49 +0100

    Added static pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def about
+  end
+
+  def services
+  end
+
+  def career
+  end
+
+  def pricing
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
+  get '/about', to: "home#about"
+  get '/services', to: "home#services"
+  get '/career', to: "home#career"
+  get '/pricing', to: "home#pricing"
+  get '/contact', to: "home#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
